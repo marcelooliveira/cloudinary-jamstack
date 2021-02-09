814a4f5e27a8c003856a66ebfa23942350eff311
marcelo.r.oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-02-08 21:18:24 -0300

    functions folder

--- functions/[id].js
+++
@@ -1,8 +0,0 @@
-const getRooms = require('./helpers/getRooms.js');
-exports.handler = async (event) => {
-    if (event.httpMethod === 'GET') {
-        return await getRooms(event);
-    } else {
-        return formattedReturn(405, {});
-    }
-};
